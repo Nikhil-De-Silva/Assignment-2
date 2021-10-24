7b6b232234aaa4a35a1afca6872213d40c99b009
Nikhil De Silva <74589155+Nikhil-De-Silva@users.noreply.github.com>
2021-10-23 21:49:28 -0700

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,39 @@
+var vg_1 = "map.vg.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_1 = "daily_cost_bar_chart.vg.json";
+vegaEmbed("#bar_chart2", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_1 = "white_wine_chart.vg.json";
+vegaEmbed("#bar_chart3", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_1 = "red_wine_chart.vg.json";
+vegaEmbed("#bar_chart4", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_1 = "area_chart.vs.json";
+vegaEmbed("#bar_chart6", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_1 = "4_bar_graphs.vs.json";
+vegaEmbed("#bar_chart5", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_1 = "2d_histogram.vs.json";
+vegaEmbed("#bar_chart7", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_1 = "area_chart2.vs.json";
+vegaEmbed("#bar_chart8", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
